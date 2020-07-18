fe4ad13e0dbd1799045a45aa7efe24dcd7c738fb
Mario <mario.tonelli@driven.cx>
2020-07-18 02:42:23 -0300

    Chamando classes certas para o css

--- site/src/componentes/Header/index.js
+++ site/src/componentes/Header/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 class Header extends React.Component{
   render(){
     return(
-      <div className="headerSite">
-        <li className="header__item">Home</li>
-        <li className="header__item">Portifólio</li>
-        <li className="header__item">Currículo</li>
-        <li className="header__item">Redes sociais</li>
+      <div className="headerMario">
+        <ul className="headerMario-lista">
+          <li className="headerMario-item">Home</li>
+          <li className="headerMario-item">Portifólio</li>
+          <li className="headerMario-item">Currículo</li>
+          <li className="headerMario-item">Redes sociais</li>
+        </ul>
       </div>
     )
   }
